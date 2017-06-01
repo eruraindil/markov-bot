eac0d33f61b12e24d911b1b6c98fb3af4b0b33ae
Matthew Roberts <contact@mroberts.me>
2017-05-31 20:28:29 -0600

    version 2 reading in whole history on start, creating file, this can be scheduled, app should be run on schedule too

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 // app.js
+// adapted from https://medium.com/@bmorelli25/build-a-simple-twitter-bot-with-node-js-in-just-38-lines-of-code-ed92db9eb078
 var Twitter = require('twitter');
 var MarkovChain = require('markovchain');
+var Jsonfile = require('jsonfile');
+var fs = require('fs');
 
 var config = require('./config.js');
 var T = new Twitter(config);
 
-var useUpperCase = function(wordList) {
-  var tmpList = Object.keys(wordList).filter(function(word) {
-    return word[0] >= 'A' && word[0] <= 'Z'
-  })
-  return tmpList[~~(Math.random()*tmpList.length)]
-}
+try {
+  fs.accessSync('./tweets.json', fs.R_OK);
+  var tweets = Jsonfile.readFileSync('./tweets.json');
 
-var stopAfter139Chars = function(sentence) {
-  return sentence.length >= 139
-}
-
-var normFn = function(word) {
-  return word.replace(/[@\'\(\)']/ig, '')
-    .replace(/(?:https?|ftp):\/\/[\n\S]+/ig, '')
-    .replace(/\&amp;/ig, '&');
-}
-
-// Set up your search parameters
-var params = {
-  screen_name: 'eruraindil',
-  count: 200,
-  include_rts: false,
-  lang: 'en'
-}
-
-var quotes = new MarkovChain('Hello world', normFn);
-
-T.get('statuses/user_timeline', params, function(err, data, response) {
-  if(!err){
-  //  console.log(data.length);
-  //  console.log(data);
-    for(var i in data){
-      quotes.parse(data[i].text);
-      // console.log(data[i].text);
-    }
-
-    // console.log(quotes.start(useUpperCase).end(stopAfter139Chars).process());
-
-    T.post('statuses/update', {status: quotes.start(useUpperCase).end(stopAfter139Chars).process() + '.'},  function(error, tweet, response) {
-      if(error) throw error;
-      console.log(tweet);  // Tweet body. 
-      // console.log(response);  // Raw response object. 
-    });
-} else {
-    console.log(err);
+  var useUpperCase = function(wordList) {
+    var tmpList = Object.keys(wordList).filter(function(word) {
+      return word[0] >= 'A' && word[0] <= 'Z'
+    })
+    return tmpList[~~(Math.random()*tmpList.length)]
   }
-})
+
+  var stopAfter139Chars = function(sentence) {
+    return sentence.length >= 139
+  }
+
+  var normFn = function(word) {
+    return word.replace(/[@\'\(\)']/ig, '')
+      .replace(/\s\s/ig, '')
+      .replace(/(?:https?|ftp):\/\/[\n\S]+/ig, '')
+      .replace(/\&amp;/ig, '&');
+  }
+
+  var quotes = new MarkovChain('Hello world', normFn);
+
+  for(var key in tweets){
+    if (tweets.hasOwnProperty(key)) {
+      quotes.parse(tweets[key]);
+    }
+  }
+  
+  var tweetText = quotes.start(useUpperCase).end(stopAfter139Chars).process() + '.';
+
+  if(!process.env.IS_DEV) {
+    T.post('statuses/update', {status: tweetText},  function(error, tweet, response) {
+      if(error) throw error;
+    });
+  } else {
+    console.log(tweetText);  // Tweet body
+  }
+} catch(e) {
+  console.log(e.stack);
+}
---
+++ genTweets.js
@@ -0,0 +1,82 @@
+// genTweets.js
+var Twitter = require('twitter');
+var Jsonfile = require('jsonfile');
+var Promise = require('bluebird');
+var fs = require('fs');
+
+var config = require('./config.js');
+var T = new Twitter(config);
+var tweets = {};
+try {
+  fs.accessSync('./tweets.json', fs.W_OK);
+  tweets = Jsonfile.readFileSync('./tweets.json');
+} catch(e) {
+  console.log(e.stack);
+}
+var results = [];
+var count = 0;
+
+// recursive functions adapted from http://www.nickstefan.net/blog/view/twitter-api
+function twitterSearchAsync(options) {
+  return new Promise(function(resolve,reject){
+    T.get('statuses/user_timeline',options,function(err, data, response){
+      console.log("success twitter", data.length);
+      resolve(data);
+    });
+  });
+}
+
+function getMaxHistory (data) {
+  var max_id, options, oldest, newest;
+  if (data.length > 0) {
+    // get oldest tweet
+    max_id = data[data.length - 1].id - 1;
+    options = {
+      screen_name: 'eruraindil',
+      count: 200,
+      include_rts: false,
+      lang: 'en',
+      max_id: max_id
+    }
+    newest = data[0].created_at;
+    oldest = data[data.length - 1].created_at;
+
+    results = results.concat(data);
+  }
+
+  // this isn't entirely necessary, but its nice to see when debugging and first trying this out
+  // each request you can see for yourself that the oldest and newest are going back in time
+  // to the next set of 200 tweets
+  count++;    
+  console.log("requests ", count, max_id, oldest, newest, "\n");
+
+  // if theres no more tweets being returned, break recursion
+  if (data.length < 2) {
+    // do stuff with your tweets 
+    
+    for(var i in results) {
+      tweets[results[i].id] = results[i].text;
+    }
+
+    var keys = Object.keys(tweets);
+    var len = keys.length;
+
+    if(process.env.IS_DEV) console.log(tweets);
+    console.log(len);
+
+    Jsonfile.writeFileSync('./tweets.json', tweets);
+
+  } else {
+    twitterSearchAsync(options).then( getMaxHistory );
+  }
+}
+
+// Set up your search parameters
+var options = {
+  screen_name: 'eruraindil',
+  count: 200,
+  include_rts: false,
+  lang: 'en'
+}
+
+twitterSearchAsync(options).then( getMaxHistory );
